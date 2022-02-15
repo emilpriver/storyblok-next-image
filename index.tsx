d64c8d7a69f8e980dd6f6e4d81b22d9c39d0fcfa
Emil Privér <emil@rivercode.se>
2022-02-15 08:56:34 +0100

    Build a storyblok next image

---
+++ index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image, { ImageLoader, ImageProps } from 'next/image';
+
+export const transformImage = (
+    src: string,
+    widthAndHeight: string,
+): string => {
+  if (!src || !widthAndHeight) return '';
+
+  const imageService = 'https://img2.storyblok.com/';
+  const path = src
+      // removed domain which don't support image transformation
+      .replace('a.storyblok.com', '')
+      // remove protocol via regex
+      .replace(/(^\w+:|^)\/\//, '');
+  return imageService + widthAndHeight + path;
+};
+
+const StoryBlokImageLoader: ImageLoader = ({
+                                             src, quality, width,
+                                           }) => {
+  // Image width and height
+  const widthAndHeight = `${width ?? 0}x0`;
+
+  // Adding quality to the image
+  const imageModifications = `filters:quality(${quality ?? 75})`;
+
+  return transformImage(src, `${widthAndHeight}/${imageModifications}`);
+};
+
+const StoryblokImage: React.FC<ImageProps> = (
+    props,
+    // eslint-disable-next-line jsx-a11y/alt-text
+) => <Image {...props} loader={StoryBlokImageLoader} />;
+
+export default StoryblokImage;
