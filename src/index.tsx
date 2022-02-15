43dc3d769e75f7ed6281ec5bd42423954a21f1f2
Emil Privér <emil@rivercode.se>
2022-02-15 09:10:11 +0100

    worked on building

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { FC } from 'react';
 import Image, { ImageLoader, ImageProps } from 'next/image';
 
-export const transformImage = (
-    src: string,
-    widthAndHeight: string,
-): string => {
+export const transformImage = (src: string, widthAndHeight: string): string => {
   if (!src || !widthAndHeight) return '';
 
   const imageService = 'https://img2.storyblok.com/';
   const path = src
-      // removed domain which don't support image transformation
-      .replace('a.storyblok.com', '')
-      // remove protocol via regex
-      .replace(/(^\w+:|^)\/\//, '');
+    // removed domain which don't support image transformation
+    .replace('a.storyblok.com', '')
+    // remove protocol via regex
+    .replace(/(^\w+:|^)\/\//, '');
   return imageService + widthAndHeight + path;
 };
 
-const StoryBlokImageLoader: ImageLoader = ({
-                                             src, quality, width,
-                                           }) => {
+const StoryBlokImageLoader: ImageLoader = ({ src, quality, width }) => {
   // Image width and height
   const widthAndHeight = `${width ?? 0}x0`;
 
@@ -28,9 +23,10 @@ const StoryBlokImageLoader: ImageLoader = ({
   return transformImage(src, `${widthAndHeight}/${imageModifications}`);
 };
 
-const StoryblokImage: React.FC<ImageProps> = (
-    props,
-    // eslint-disable-next-line jsx-a11y/alt-text
+const StoryblokImage: FC<ImageProps> = (
+  props,
+  // eslint-disable-next-line jsx-a11y/alt-text
+// eslint-disable-next-line react/jsx-props-no-spreading
 ) => <Image {...props} loader={StoryBlokImageLoader} />;
 
 export default StoryblokImage;
