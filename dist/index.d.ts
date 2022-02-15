a5f2566c74ca2b4645709c4918d71b9e15cc66eb
Emil Privér <emil@rivercode.se>
2022-02-15 09:24:53 +0100

    Removed dist from gitignore

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+import { ImageProps } from 'next/image';
+export declare const transformImage: (src: string, widthAndHeight: string) => string;
+declare const StoryblokImage: FC<ImageProps>;
+export default StoryblokImage;
